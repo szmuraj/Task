c3e24044f91018bff01994a7e7f23609edb351d9
szmuraj <przemyslaw.sicinski@o2.pl>
2023-01-27 16:44:43 +0100

    19.2

--- tasks/src/main/java/com/crud/tasks/TasksApplication.java
+++ tasks/src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,7 +1,10 @@
 package com.crud.tasks;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class TasksApplication {
